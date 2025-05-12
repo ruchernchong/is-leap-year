d58a784a8f50b315d6742f054870e2a0c8d012e2
Ru Chern Chong <hello@ruchern.dev>
2025-05-13 02:57:19 +0800

    Setup DaisyUI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,7 +59,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="garden">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <main className="container mx-auto">
           <Navbar />
