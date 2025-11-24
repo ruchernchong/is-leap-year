df0e81207ec14620f84ea90d0d704530c7f90af6
Ru Chern Chong <hello@ruchern.dev>
2025-11-24 13:10:20 +0800

    chore: remove lint-staged test file

--- test-lint-staged.ts
+++
@@ -1,9 +0,0 @@
-// Test file with formatting issues
-const test = "badly formatted";
-const unused = 123;
-
-function badlyFormatted(x: number) {
-  return x + 1;
-}
-
-export { test, badlyFormatted };
